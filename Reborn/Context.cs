90b215ca911d1b3f1169e0f25a207224e459cb9c
yatoenough <yatoenough@gmail.com>
2024-01-19 02:16:18 +0100

    Added connection to PostgreSQL, realised products CRUD cycle

---
+++ Reborn/Context.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reborn.Models;
+
+namespace Reborn
+{
+    public class Context : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Inventory { get; set; }
+        public DbSet<User> User { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Product>().HasKey("id");
+            builder.Entity<Order>().HasKey("id");
+            builder.Entity<User>().HasKey("id");
+
+        }
+    }
+}
+
+
---
+++ Reborn/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Reborn.Models
+{
+    [Table("products")]
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string description { get; set; }
+
+        [Required]
+        public double price { get; set; }
+
+        [Required]
+        public string color { get; set; }
+
+        [Required]
+        public string size { get; set; }
+    }
+}
