dd8aef10a19b8b425ffefe3e662e84007019e44f
Dan_Ostap <ostapenkovdanil13@gmail.com>
2024-01-19 01:19:24 +0100

    Add project files.

---
+++ Reborn/Models/Orders.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Reborn.Models
+{
+    public class Orders
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; }
+        public  int user_Id { get; set; }
+        public int product_Id { get; set; }
+        public string status { get; set; }
+        public DateOnly creation_date { get; set; }
+        public DateOnly pickup_date { get; set; }
+    }
+}
---
+++ Reborn/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Reborn.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+        public bool isActivated { get; set; } = false;
+        public string activationLink { get; set; }
+    }
+}
