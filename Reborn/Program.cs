e74c568a79767bf1bdd75aa225f9c06a9878ea74
yatoenough <yatoenough@gmail.com>
2024-01-20 01:06:58 +0100

    Refactored app by repository-service pattern

--- Reborn/Program.cs
+++ Reborn/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Reborn;
+using Reborn.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 string? connection = builder.Configuration.GetConnectionString("pg");
 builder.Services.AddDbContext<Context>(options => options.UseNpgsql(connection));
+
+builder.Services.AddScoped<IProductsService, ProductsService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
