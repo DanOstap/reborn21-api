b6f587a0d8d43ca5fc3c2a03ac552ff14eb0a2d2
Dan_Ostap <84470259+DanOstap@users.noreply.github.com>
2024-04-23 19:06:26 +0200

    Mail
    add mail service

---
+++ Reborn/Controllers/MailController.cs
@@ -0,0 +1,24 @@
+﻿using MailKit;
+using Microsoft.AspNetCore.Mvc;
+using Reborn.Services;
+using Reborn.Models;
+
+namespace Reborn.Controllers
+{
+    [ApiController]
+    [Route("Mail")]
+    public class MailController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        public MailController(IConfiguration configuration) {
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        public void SendMail(string toMail, string subject, string body) {
+            Services.MailService mailService = new Services.MailService(configuration);
+            mailService.SendEmail(toMail, subject, body);
+        }
+
+    }
+}
